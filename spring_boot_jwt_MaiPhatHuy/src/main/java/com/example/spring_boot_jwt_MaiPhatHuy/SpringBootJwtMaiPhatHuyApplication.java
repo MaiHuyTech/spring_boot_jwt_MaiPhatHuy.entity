b7b40e19744450adc99bfda2ea8293175cab1d54
maihuytech@gmail.com <maihuytech@gmail.com>
2021-09-22 14:49:32 +0700

    commit fist

---
+++ spring_boot_jwt_MaiPhatHuy/src/main/java/com/example/spring_boot_jwt_MaiPhatHuy/SpringBootJwtMaiPhatHuyApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_jwt_MaiPhatHuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtMaiPhatHuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtMaiPhatHuyApplication.class, args);
+	}
+
+}
