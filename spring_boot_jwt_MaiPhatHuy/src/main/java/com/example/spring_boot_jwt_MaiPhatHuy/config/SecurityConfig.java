a2bbfcf105f90954153097dd8a0d9e452f7d5a6b
maihuytech@gmail.com <maihuytech@gmail.com>
2021-09-24 21:49:25 +0700

    commit login last

--- spring_boot_jwt_MaiPhatHuy/src/main/java/com/example/spring_boot_jwt_MaiPhatHuy/config/SecurityConfig.java
+++ spring_boot_jwt_MaiPhatHuy/src/main/java/com/example/spring_boot_jwt_MaiPhatHuy/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.example.spring_boot_jwt_MaiPhatHuy.config;
 
-import com.example.spring_boot_jwt_MaiPhatHuy.filter.JwtRequestFilter;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,8 +14,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private JwtRequestFilter jwtRequestFilter;
+
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
@@ -25,8 +24,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/register")
                 .permitAll();
 
-        http.addFilterBefore(jwtRequestFilter,
-                        UsernamePasswordAuthenticationFilter.class)
-                .csrf().disable();
     }
 }
